01ba5b38d8a5ba5bd50402583c9c428812ab7463
sudyal <sudyalai101@gmail.com>
2024-09-11 16:17:30 +0530

    Done

--- header/search/index.js
+++ header/search/index.js
@@ -5,4 +5,12 @@ $(() => {
     $(".navmenu").toggleClass("active");
     $(".hamburger").toggleClass("active");
   });
+
+  window.onscroll = () => {
+    if (window.scrollY > 100) {
+      $("header").addClass("active");
+    } else {
+      $("header").removeClass("active");
+    }
+  };
 });
